4b438f4621e91a0623de55ede957e92252ac9a0c
Mxhran-m <mehranmulla367@gmail.com>
2025-02-13 22:52:21 +0530

    created controller and route

--- src/app.js
+++ src/app.js
@@ -14,4 +14,7 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from './routes/user.router.js' // import here only!!
+app.use("/api/v1/user", userRouter)
+
 export { app }
