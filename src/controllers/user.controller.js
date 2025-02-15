0f67dce7e4741577f13c19acab372602f14ae37f
Mxhran-m <mehranmulla367@gmail.com>
2025-02-15 09:47:00 +0530

    bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,7 +52,7 @@ const registerUser = asyncHandler( async (req, res) => {
         coverImage: coverImage?.url || ""
     })
 
-    const createdUser = await user.findById(user._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password  -refreshToken"
     )
 
